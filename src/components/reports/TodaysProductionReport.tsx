163a0fd0bd7b6260acf6f9d7ae56bb06723cf599
Flirty Guju <deathmaniya@gmail.com>
2025-08-27 13:59:27 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Input type="numbe

--- src/components/reports/TodaysProductionReport.tsx
+++ src/components/reports/TodaysProductionReport.tsx
@@ -160,7 +160,6 @@ export default function TodaysProductionReport() {
             if (!summary[operator]) {
                 summary[operator] = { operator, pcs: 0, date: format(parseISO(l.returnDate!), 'PP') };
             }
-            // Prioritize subPacketCount, fall back to packetCount
             summary[operator].pcs += l.subPacketCount ?? l.packetCount;
         });
         
