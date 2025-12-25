d6e97e74984e72d6e0e6c1055081d36ae422688e
Flirty Guju <deathmaniya@gmail.com>
2025-12-25 13:16:47 +0000

    make mian js with using this code
    ------------------------------------

---
+++ main.js
@@ -0,0 +1,32 @@
+
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  if (app.isPackaged) {
+    win.loadFile(path.join(__dirname, 'out', 'index.html'));
+  } else {
+    win.loadURL('http://localhost:3000');
+    win.webContents.openDevTools(); // dev में tools open होंगे
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
